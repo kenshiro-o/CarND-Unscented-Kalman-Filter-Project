950c47a4aa087b6e72e5ca6d2cb8d2aa42a7485d
Eddie Forson <eddie.forson@gmail.com>
2018-02-06 08:10:46 +0000

    Added implementation of RMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,49 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  // We have a vector rmse that will store the RMSE values for:
+  // (rmse_pos_x, rmse_pos_y, rmse_v_x, rmse_v_y)
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  int estSize = estimations.size();
+  int gtSize = ground_truth.size();
+
+  // Size of estimations and ground truth vectors should be poisitive
+  if (estSize == 0 || gtSize == 0)
+  {
+    cout << "Size of estimations or ground truth vector 0:" << std::endl;
+    return rmse;
+  }
+
+  // They should also match...
+  if (estSize != gtSize)
+  {
+    cout << "Size of estimations and ground truth vectors differ." << std::endl;
+    return rmse;
+  }
+
+  //accumulate squared residuals
+  for (int i = 0; i < estSize; ++i)
+  {
+    VectorXd est = estimations.at(i);
+    VectorXd g = ground_truth.at(i);
+
+    // cout << "Estimations: " << est << std::endl;
+    // cout << "Ground Tuth: " << g << std::endl;
+
+    VectorXd res = est - g;
+    VectorXd res_pow = res.array().square();
+    res_pow /= estSize;
+
+    // cout << "Res Pow: " << res_pow
+    //      << std::endl;
+    rmse = rmse + res_pow;
+  }
+
+  rmse = rmse.array().sqrt();
+
+  // cout << "RMSE = " << rmse << std::endl;
+
+  return rmse;
 }
